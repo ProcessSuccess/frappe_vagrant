07dfbccf49d5ee0c32cbac1bbe72bfab03ba3083
Derek Brenner <derekbrenner@gmail.com>
2017-06-21 19:45:59 -0700

    adding

--- Vagrantfile
+++ Vagrantfile
@@ -2,12 +2,15 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "bento/ubuntu-16.04"
+  config.vm.box = "ubuntu/trusty64"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network "forwarded_port", guest: 8000, host: 8080
   config.vm.network "forwarded_port", guest: 3000, host: 3000
+  #config.vm.boot_timeout = 900
   config.vm.synced_folder "Frappe", "/home/vagrant"
+
   config.vm.provider "virtualbox" do |vb|
+    #vb.customize ["modifyvm", :id, "--cableconnected1", "on"]
     vb.name = "Frappe"  
     vb.memory = "3000"
   end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
+echo "Turn on some Death Metal and go make a pot of coffee!"
 apt-get update
 cd /vagrant/Frappe
 sudo apt install python2.7
-echo "alias python='python2.7'" >> ~/.bashrc && source ~/.bashrc
+echo "hello"
+echo "getin123" | ./your_script
 wget https://raw.githubusercontent.com/frappe/bench/master/playbooks/install.py
-#sudo python install.py --develop
-python install.py --develop --user vagrant
-
-
-ln -s /home/frappe/frappe-bench/apps/process_success/process_success/public /home/frappe/frappe-bench/sites/assets/process_success
+printf 'getin123\ngetin123\ngetin123\ngetin123\n' | python install.py --develop --user vagrant
+#ln -s /home/frappe/frappe-bench/apps/process_success/process_success/public /home/frappe/frappe-bench/sites/assets/process_success
