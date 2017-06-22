dc42104f7a1fb52c3d9784cb2b705b264b55f450
Derek Brenner <derekbrenner@gmail.com>
2017-06-21 17:12:16 -0700

    change

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 apt-get update
 cd /vagrant/Frappe
+sudo apt install python2.7
+echo "alias python='python2.7'" >> ~/.bashrc && source ~/.bashrc
 wget https://raw.githubusercontent.com/frappe/bench/master/playbooks/install.py
 #sudo python install.py --develop
+python install.py --develop --user vagrant
 
+
+ln -s /home/frappe/frappe-bench/apps/process_success/process_success/public /home/frappe/frappe-bench/sites/assets/process_success
