796f7353af6ea3ce290f4149c51c8043281630d0
Derek Brenner <derekbrenner@gmail.com>
2017-06-22 15:26:54 -0700

    changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,10 @@ echo "Turn on some Death Metal and go make a pot of coffee!"
 . /home/vagrant/vagrant_setup_info.sh
 
 apt-get update
-cd /vagrant/Frappe
+cd /vagrant/frappe-bench
 sudo apt install python2.7
 wget https://raw.githubusercontent.com/frappe/bench/master/playbooks/install.py
-printf '$MARIA_DB_PASS\n$MARIA_DB_PASS\n$ADMIN_PASS\n$ADMIN_PASS\n' | python install.py --develop --user vagrant
+printf $MARIA_DB_PASS"\n"$MARIA_DB_PASS"\n"$ADMIN_PASS"\n"$ADMIN_PASS"\n" | python install.py --develop --user vagrant
 cd /home/vagrant/frappe-bench
 
 cd /home/vagrant/frappe-bench/apps/frappe
@@ -27,21 +27,48 @@ bench get-app https://$GIT_USER_NAME:$GIT_PASS@github.com/process-success/Proces
 ln -s /home/vagrant/frappe-bench/apps/process_success/process_success/public /home/vagrant/frappe-bench/sites/assets/process_success
 
 cd /home/vagrant/frappe-bench/apps/process_success
-sudo npm install --global gulp-cli
-npm install
+# Save credentials
+git config credential.helper store
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_FULL_NAME
+#git remote add origin https://$GIT_USER_NAME:$GIT_PASS@github.com/process-success/Process-Success-Application.git
+git checkout develop
+
+cd /home/vagrant/frappe-bench
+
+printf $MARIA_DB_PASS"\n"$WEB_ADMIN_PASS"\n"$WEB_ADMIN_PASS | bench new-site crv.develop
+bench --site crv.develop install-app process_success
+bench use crv.develop
+bench migrate
+
+#cd /home/vagrant/frappe-bench/apps/process_success
+#sudo npm install --global gulp-cli
+#npm install
 
 echo "--------------------------------------"
 echo "         INSTALATION COMPLETE "
 echo "--------------------------------------"
-echo "Run The Following Comands"
-echo " "
+echo "-----------------------"
+echo "- Start Up the Server -"
+echo "-----------------------"
 echo "$  vagrant ssh"
 echo "Enter Password: vagrant"
-echo "                             "
 echo "$  cd /home/vagrant/frappe-bench"
-echo "$  bench new-site crv.develop"
-echo "$  bench --site crv.develop install-app process_success"
-echo "$  bench use crv.develop"
 echo "$  bench start"
+echo " "
+echo "-----------------"
+echo "- Starting gulp -"
+echo "---------------- "
+echo "Open a new terminal "
+echo "$  vagrant ssh"
+echo "Enter Password: vagrant"
+echo "$  sudo npm install --global gulp-cli"
+echo "$  npm install"
+echo "$  gulp"
+
+
+#echo "$  bench new-site crv.develop"
+#echo "$  bench --site crv.develop install-app process_success"
+#echo "$  bench use crv.develop"
 
 #ln -s /home/frappe/frappe-bench/apps/process_success/process_success/public /home/frappe/frappe-bench/sites/assets/process_success
--- vagrant_setup_info.sh
+++ vagrant_setup_info.sh
@@ -1,5 +1,18 @@
+# SQL password
 MARIA_DB_PASS="getin123"
+
+# Not sure what this is for
+# required and piped into the python install script 
 ADMIN_PASS="getin123"
+
+# password for the site once live
+# username : Administrator
 WEB_ADMIN_PASS="getin123"
+
+# Git Login Info
+# not an email address
+# !! be carefull not to check this in with info !!
 GIT_USER_NAME=""
 GIT_PASS=""
+GIT_EMAIL=""
+GIT_FULL_NAME=""
